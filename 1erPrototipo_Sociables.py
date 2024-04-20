429b089d148564eea9913b0bfe116d90a9631188
UnknownProgrammer-Ag <75504548+UnknownProgrammer-Ag@users.noreply.github.com>
2024-04-20 16:36:38 -0300

    Primer Prototipo

---
+++ 1erPrototipo_Sociables.py
@@ -0,0 +1,63 @@
+from math import sqrt
+from time import time
+
+
+def delta_time(func):
+    def wrap_func(*args, **kwargs):
+        start = time()
+        result = func(*args, **kwargs)
+        end = time()
+        print(f'Function {func.__name__!r} executed in {(end-start):.4f}s')
+        return result
+    return wrap_func
+
+
+def divisores(entero):
+    suma = 1
+    div = 2
+    cota_sup = sqrt(entero)
+    # print(cota_sup, '\n')
+    while div <= cota_sup:
+
+        if (entero % div) == 0:
+            # print(div, '\n')
+            suma += (div+(entero/div))
+            # print(suma, '\n')
+        div += 1
+    return suma
+
+# Tomando el periodo 4, se deben recuperar 4 numeros
+
+
+def sociables(periodo, numero):
+    tupla4 = []
+    while periodo > 0:
+        if tupla4 != []:
+            if numero == tupla4[0]:
+                break
+        tupla4.append(numero)
+        numero = divisores(numero)
+        periodo -= 1
+
+    if len(tupla4) < 4:
+        tupla4 = []
+    else:
+        if numero != tupla4[0]:
+            tupla4 = []
+    return tupla4
+
+
+@delta_time
+def principal():
+    conjuntos = []
+    for i in range(20000):  # Si prueban con 1300000 va a tardar lo suyo
+        social = sociables(4, i)  # Periodo 5 y rango de 20000 tambien sirve
+        if social != []:
+            conjuntos.append(social)
+    if conjuntos == []:
+        print("No se encontraron conjuntos")
+    else:
+        print(conjuntos)
+
+
+principal()
