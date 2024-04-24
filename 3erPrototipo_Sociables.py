6e7af29f8753c7cf635da6df75ba565abd318e91
UnknownProgrammer-Ag <75504548+UnknownProgrammer-Ag@users.noreply.github.com>
2024-04-24 19:30:46 -0300

    Falta: Eliminar grupos amigos en periodos pares

--- 3erPrototipo_Sociables.py
+++ 3erPrototipo_Sociables.py
@@ -3,29 +3,28 @@ from delta import delta_time
 
 @delta_time
 def sociables():
-    #Adaptarlo para que tenga la compactacion de 2do prototipo y el uso de listas del primer prototipo
     periodo = int(input('Ingrese el periodo '))
     rango = int(input('Ingrese de que rango de numeros desea encontrar los conjuntos sociables existentes '))
     conjuntos_posibles = set()
     for i in range(rango):
-       elemento=i
-       subconjunto = []
-       bucle=periodo
-       while (len(subconjunto) <periodo) and (bucle>0):
-     ▏   sumaDiv=1 #Sabemos que un numero siempre tendra de divisor a su identidad
-     ▏   div=2
-     ▏   divMax=sqrt(elemento)
-    ▏   while div <= divMax:
-    ▏       if (elemento % div) == 0:
-     ▏         sumaDiv += (div+(elemento//div))
-     ▏         div += 1
-         subconjunto.append(elemento)
-         elemento=sumaDiv
-   ▏   ▏  bucle-=1
-   ▏   ▏   if (int(i)==elemento):
-     ▏   ▏   ▏   conjuntos_posibles.add(frozenset(subconjunto))
-     ▏
-   ▏   if bool(conjuntos_posibles):
-     ▏   ▏   print('Para ',rango,' numeros, existen estos conjuntos de periodo ',periodo,' :',[i for i in conjuntos
-     ▏   else:
-     ▏   ▏   print('No se encontraron conjuntos')
+        elemento = i
+        subconjunto = []
+        bucle = periodo
+        while (len(subconjunto)<periodo) and (bucle>0):
+            sumaDiv = 1 #Sabemos que un numero siempre tendra de divisor a su identidad
+            div = 2
+            divMax = sqrt(elemento)
+            while div <= divMax:
+                if (elemento % div) == 0:
+                    sumaDiv += (div+(elemento//div))
+                div +=1
+            #Ver alguna forma de verificar que los conjuntos no sean nros amigos repetidos en periodo para
+            subconjunto.append(elemento)
+            elemento = sumaDiv
+            bucle -=1
+            if (int(i)==elemento):
+                conjuntos_posibles.add(tuple(subconjunto))
+    if conjuntos_posibles:
+        print('Para ',rango,' numeros, existen estos conjuntos de periodo ',periodo,' :',[i for i in conjuntos_posibles if (len(i)==periodo)and(i[0]!=i[1])])
+
+sociables()
--- 3erPrototipo_Sociables_BIS.py
+++
@@ -1,5 +0,0 @@
-from math import sqrt
-from delta import delta_time
-
-@delta_time
-def sociables():
