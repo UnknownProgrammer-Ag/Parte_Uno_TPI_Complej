f5fb7ff58b9ff679576f69850109f7de1a5f83d4
UnknownProgrammer-Ag <75504548+UnknownProgrammer-Ag@users.noreply.github.com>
2024-04-25 14:13:11 -0300

    Ultimos Pulimentos

--- 3erPrototipo_Sociables.py
+++ 3erPrototipo_Sociables.py
@@ -1,30 +1,34 @@
 from math import sqrt
 from delta import delta_time
 
+
 @delta_time
 def sociables():
     periodo = int(input('Ingrese el periodo '))
-    rango = int(input('Ingrese de que rango de numeros desea encontrar los conjuntos sociables existentes '))
+    rango = int(input(
+        'Ingrese de que rango de numeros desea encontrar los conjuntos sociables existentes '))
     conjuntos_posibles = set()
     for i in range(rango):
         elemento = i
         subconjunto = []
         bucle = periodo
-        while (len(subconjunto)<periodo) and (bucle>0):
-            sumaDiv = 1 #Sabemos que un numero siempre tendra de divisor a su identidad
+        while (len(subconjunto) < periodo) and (bucle > 0):
+            sumaDiv = 1  # Sabemos que un numero siempre tendra de divisor a su identidad
             div = 2
             divMax = sqrt(elemento)
             while div <= divMax:
                 if (elemento % div) == 0:
                     sumaDiv += (div+(elemento//div))
-                div +=1
-            #Ver alguna forma de verificar que los conjuntos no sean nros amigos repetidos en periodo para
+                div += 1
+            # Ver alguna forma de verificar que los conjuntos no sean nros amigos repetidos en periodo para
             subconjunto.append(elemento)
             elemento = sumaDiv
-            bucle -=1
-            if (int(i)==elemento):
+            bucle -= 1
+            if (int(i) == elemento) and (len(subconjunto) == periodo) and (subconjunto[0] != subconjunto[periodo-1]):
                 conjuntos_posibles.add(tuple(subconjunto))
     if conjuntos_posibles:
-        print('Para ',rango,' numeros, existen estos conjuntos de periodo ',periodo,' :',[i for i in conjuntos_posibles if (len(i)==periodo)and(i[0]!=i[1])])
+        print('Para ', rango, ' numeros, existen estos conjuntos de periodo ',
+              periodo, ' :', conjuntos_posibles)
+
 
 sociables()
