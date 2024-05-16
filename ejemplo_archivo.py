94e61b7e3cc83959f6e81c26a494aa85f6481eff
UnknownProgrammer-Ag <75504548+UnknownProgrammer-Ag@users.noreply.github.com>
2024-05-16 13:49:54 -0300

    Transcripcion

--- ejemplo_archivo.py
+++ ejemplo_archivo.py
@@ -1,11 +1,51 @@
 from decorators import delta_time
 
 
-@delta_time("GRUPO GN")
-def sociables(DATOS):
-	
-	return lista
+@delta_time("GRUPO G1")
+def sociables(N, PERIODOS):
+    def descomponer(elemento, cache, primos):
+        if elemento in cache:
+            return cache[elemento]
+
+        cont = 0
+        sumaDiv = 1
+        div = 2
+        divMax = (elemento)**(0.5)
+        while div <= divMax:
+            if (elemento % div) == 0:
+                sumaDiv += (div+(elemento//divMax))
+                cont += 1
+            div += 1
+
+        if cont >= 4:
+            cache[elemento] = sumaDiv
+        if sumaDiv == 1:
+            primos[elemento] = 1
+
+        return sumaDiv
+
+    lista = set()
+    cache = {}
+    primos = {}
+
+    for i in range(N):
+        if i in cache or i in primos:
+            continue
+
+        elemento = i
+        subconjunto = []
+        visitados = set()
+
+        while (len(subconjunto) < periodo) and (elemento not in visitados):
+            visitados.add(elemento) subconjunto.append(elemento)
+            if elemento in primos:
+                continue
+            elemento = descomponer(elemento, cache, primos)
+
+        if (i == elemento) and (len(visitados) == periodo) and (subconjunto[0] != subconjunto[periodo-1])
+    return lista
+
 
 if __name__ == "__main__":
-	#INGRESO DATOS
-	print(sociables(DATOS))
+    # INGRESO DATOS
+    print(sociables(DATOS))
