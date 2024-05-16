d78ddbe77e641963a1427792d666c2625404861c
UnknownProgrammer-Ag <75504548+UnknownProgrammer-Ag@users.noreply.github.com>
2024-05-16 19:56:25 -0300

    Entrega realizada

--- G1.py
+++ G1.py
@@ -1,33 +1,37 @@
 from decorators import delta_time
 
 
-@delta_time  # "Grupo G1"
-def sociables(N, PERIODOS):
-    def descomponer(elemento, cache, primos):
-        if elemento in cache:
-            return cache[elemento]
+def descomponer(elemento, cache, primos):
+    if elemento in cache:
+        return cache[elemento]
 
-        cont = 0
-        sumaDiv = 1
-        div = 2
-        divMax = (elemento)**(0.5)
-        while div <= divMax:
-            if (elemento % div) == 0:
-                sumaDiv += (div+(elemento//divMax))
-                cont += 1
-            div += 1
+    cont = 0
+    sumaDiv = 1  # Sabemos que un numero siempre tendra de divisor a su identidad
+    div = 2
+    divMax = (elemento)**(0.5)
+    while div <= divMax:
+        if (elemento % div) == 0:
+            sumaDiv += (div+(elemento//div))
+            cont += 1
+        div += 1
 
-        if cont >= 4:
-            cache[elemento] = sumaDiv
-        if sumaDiv == 1:
-            primos[elemento] = 1
+    if cont >= 4:
+        cache[elemento] = sumaDiv
+    if sumaDiv == 1:
+        primos[elemento] = 1
 
-        return sumaDiv
+    return sumaDiv
 
-    def conjuntos(N, periodo):
-        lista = set()
+
+@delta_time("GRUPO G1")
+def sociables(N, periodos):
+    lista = []
+    for periodo in periodos:
+
+        conjuntos_posibles = set()
         cache = {}
         primos = {}
+
         for i in range(N):
             if i in cache or i in primos:
                 continue
@@ -35,7 +39,9 @@ def sociables(N, PERIODOS):
             elemento = i
             subconjunto = []
             visitados = set()
+
             while (len(subconjunto) < periodo) and (elemento not in visitados):
+
                 visitados.add(elemento)
                 subconjunto.append(elemento)
                 if elemento in primos:
@@ -43,14 +49,16 @@ def sociables(N, PERIODOS):
                 elemento = descomponer(elemento, cache, primos)
 
             if (i == elemento) and (len(visitados) == periodo) and (subconjunto[0] != subconjunto[periodo-1]):
-                lista.add(tuple(sorted(subconjunto)))
+                conjuntos_posibles.add(tuple(sorted(subconjunto)))
 
-        return lista
+        if conjuntos_posibles:
+            # print('Para', rango, 'numeros, existen estos conjuntos de periodo',
+            # 	periodo, ':', conjuntos_posibles)
+            lista.append(conjuntos_posibles)
 
-    for periodo in PERIODOS:
-        print(conjuntos(N, periodo))
+    return lista
 
 
 if __name__ == "__main__":
     # INGRESO DATOS
-    print(sociables(100000, [5, 6, 7]))
+    print(sociables(N, periodos))
