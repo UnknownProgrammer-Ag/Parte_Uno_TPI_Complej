ed1ac333a77fe92b7cffc645cc0ddf00afa5eab3
UnknownProgrammer-Ag <75504548+UnknownProgrammer-Ag@users.noreply.github.com>
2024-05-16 16:12:58 -0300

    Todavia no Funciona:(

--- G1.py
+++ G1.py
@@ -1,30 +1,27 @@
 from decorators import delta_time
 
+def descomponer(elemento, cache, primos):
+    if elemento in cache:
+        return cache[elemento]
+    
+    cont = 0
+    sumaDiv = 1
+    div = 2
+    divMax = (elemento)**(0.5)
+    while div <= divMax:
+        if (elemento % div) == 0:
+            sumaDiv += (div+(elemento//divMax))
+            cont += 1
+        div += 1
+    if cont >= 4:
+        cache[elemento] = sumaDiv
+    if sumaDiv == 1:
+        primos[elemento] = 1
+    return sumaDiv
 
 @delta_time  # "Grupo G1"
 def sociables(N, PERIODOS):
-    def descomponer(elemento, cache, primos):
-        if elemento in cache:
-            return cache[elemento]
-
-        cont = 0
-        sumaDiv = 1
-        div = 2
-        divMax = (elemento)**(0.5)
-        while div <= divMax:
-            if (elemento % div) == 0:
-                sumaDiv += (div+(elemento//divMax))
-                cont += 1
-            div += 1
-
-        if cont >= 4:
-            cache[elemento] = sumaDiv
-        if sumaDiv == 1:
-            primos[elemento] = 1
-
-        return sumaDiv
-
-    def conjuntos(N, periodo):
+    for periodo in PERIODOS:
         lista = set()
         cache = {}
         primos = {}
@@ -45,11 +42,7 @@ def sociables(N, PERIODOS):
             if (i == elemento) and (len(visitados) == periodo) and (subconjunto[0] != subconjunto[periodo-1]):
                 lista.add(tuple(sorted(subconjunto)))
 
-        return lista
-
-    for periodo in PERIODOS:
-        print(conjuntos(N, periodo))
-
+        print(lista)
 
 if __name__ == "__main__":
     # INGRESO DATOS
